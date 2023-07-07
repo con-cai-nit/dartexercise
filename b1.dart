1ecb78e1aac5773215d2a690edc43308702fbd64
= <hoangngocanhnl11@gmail.com>
2023-07-07 23:38:23 +0700

    ex

---
+++ b1.dart
@@ -0,0 +1,7 @@
+/*
+Tạo biến với các kiểu dữ liệu khác nhau và gán giá trị cho chúng.
+*/
+
+void main() {
+  //code
+}
---
+++ main.dart
@@ -0,0 +1,76 @@
+/*
+Bài tập sử dụng Các toán tử và các biểu thức trong Dart
+Dưới đây là một số bài tập về việc sử dụng các toán tử và các biểu thức trong Dart:
+
+Tính tổng của hai số nguyên a và b và in ra kết quả.
+Tìm giá trị nhỏ nhất của ba số nguyên a, b, và c.
+Tìm giá trị lớn nhất của ba số nguyên a, b, và c.
+Tìm giá trị trung bình của ba số nguyên a, b, và c.
+Viết chương trình kiểm tra xem một số nguyên có phải là số chẵn hay không.
+Viết chương trình kiểm tra xem một số nguyên có phải là số nguyên tố hay không.
+Viết chương trình tính bậc hai của một số nguyên.
+Viết chương trình tính giai thừa của một số nguyên.
+Viết chương trình kiểm tra xem một năm có phải là năm nhuận hay không.
+Viết chương trình kiểm tra xem một chuỗi có phải là chuỗi palindrome hay không.
+10 + Bài tập Dart về conditionals
+Viết một chương trình Dart để kiểm tra xem một số có phải là số chẵn hay là số lẻ.
+Viết một chương trình Dart để kiểm tra xem một số có phải là số âm hay là số dương.
+Viết một chương trình Dart để kiểm tra xem một số có lớn hơn hoặc bằng 0 hay không.
+Viết một chương trình Dart để tính tổng của hai số và kiểm tra xem tổng của hai số có lớn hơn hoặc bằng 0 hay không.
+Viết một chương trình Dart để tìm ra số lớn nhất trong ba số.
+Viết một chương trình Dart để tính diện tích hình tròn dựa trên bán kính và kiểm tra xem diện tích của hình tròn có lớn hơn 0 hay không.
+Viết một chương trình Dart để tính tiền thuế dựa trên tổng thu nhập và mức thuế.
+Viết một chương trình Dart để tìm ra số nhỏ nhất trong một mảng các số.
+Viết một chương trình Dart để kiểm tra xem một chuỗi có phải là chuỗi rỗng hay không.
+Viết một chương trình Dart để kiểm tra xem
+10 bài tập Dart về data structures
+Tạo một danh sách số nguyên và in ra màn hình các phần tử trong danh sách.
+Tạo một map của các đôi tượng với các trường tên và giá trị, in ra màn hình các đối tượng trong map.
+Tạo một set của các chuỗi và in ra màn hình tất cả các phần tử trong set.
+Tạo một queue và thực hiện các hoạt động thêm phần tử vào queue, xóa phần tử khỏi queue, và in ra màn hình các phần tử trong queue.
+Tạo một stack và thực hiện các hoạt động thêm phần tử vào stack, xóa phần tử khỏi stack, và in ra màn hình các phần tử trong stack.
+Tạo một linked list và thực hiện các hoạt động thêm phần tử vào linked list, xóa phần tử khỏi linked list, và in ra màn hình các phần tử trong linked list.
+Tạo một binary tree và thực hiện các hoạt động thêm phần tử vào binary tree, xóa phần tử khỏi binary tree, và in ra màn hình các phần tử trong binary tree.
+Tạo một graph và thực hiện các hoạt động thêm cạnh vào graph, xóa cạnh khỏi graph, và in ra màn hình các đỉnh và cạnh trong graph.
+10+ bài tập về loop trong Dart
+Tìm tổng của các số từ 1 đến n.
+Tìm tổng của các số chẵn từ 1 đến n.
+Tìm tổng của các số lẻ từ 1 đến n.
+In ra các số từ 1 đến n theo thứ tự ngược.
+In ra một tam giác Pascal.
+In ra một bảng cửu chương.
+In ra một bảng cửu chương theo thứ tự ngược.
+Tìm số lớn nhất trong một danh sách.
+Tìm số nhỏ nhất trong một danh sách.
+Tìm số xuất hiện nhiều nhất trong một danh sách.
+Tìm các số hoàn thiện trong một khoảng từ n đến m.
+Tìm số nguyên tố trong một khoảng từ n đến m.
+10 + bài tập OOP, interface, Object trong Dart
+Tạo một lớp Sinh viên có các thuộc tính như tên, tuổi, điểm trung bình, chuyên ngành và phương thức tính điểm trung bình.
+Tạo một lớp Khoa có các thuộc tính như tên khoa, danh sách các môn học và phương thức in ra danh sách môn học.
+Tạo một lớp Giáo viên có các thuộc tính như tên giáo viên, danh sách các môn dạy và phương thức in ra danh sách môn dạy.
+Tạo một interface Shape với phương thức tính diện tích và chu vi.
+Tạo lớp hình tròn, hình chữ nhật và hình tam giác kế thừa từ interface Shape.
+Tạo một lớp Animal có các thuộc tính như tên động vật, số chân và phương thức giống nhau.
+Tạo lớp Dog, Cat kế thừa từ lớp Animal và có các phương thức riêng.
+Tạo một lớp Vehicle có các thuộc tính như tên phương tiện, số bánh và phương thức chạy.
+Tạo lớp ô tô, xe máy kế thừa từ lớp Vehicle và có các phương thức riêng.
+10 bài tập Dart về exception handling
+Dưới đây là 10 bài tập về exception handling trong Dart:
+
+Tạo một exception class mới và ném nó khi xảy ra lỗi nhất định.
+Viết một hàm xử lý ngoại lệ để gọi hàm có thể gây ra ngoại lệ.
+Tìm cách kiểm tra một số nguyên có phải là số chẵn hay lẻ bằng cách sử dụng exception handling.
+Viết một hàm tìm kiếm từ điển bằng cách sử dụng exception handling để xử lý trường hợp tìm kiếm không thành công.
+Viết một hàm để kiểm tra xem một chuỗi có phải là số hay không bằng cách sử dụng exception handling.
+Tìm cách sử dụng exception handling để xử lý lỗi trong quá trình ghi file.
+Viết một hàm để tính toán giá trị trung bình cộng của một mảng số bằng cách sử dụng exception handling để xử lý trường hợp mảng trống.
+Sử dụng exception handling để xử lý trường hợp nhập sai dữ liệu từ người dùng.
+Sử dụng exception handling để xử lý trường hợp kết nối mạng không thành công.
+Bài tập Dart về File và API
+Đọc một tập tin văn bản và in nội dung của nó ra màn hình.
+Ghi một chuỗi vào một tập tin văn bản mới.
+Sử dụng thư viện http để truy cập một API RESTful và in ra màn hình dữ liệu đầu ra.
+Tạo một form đăng ký cho người dùng với các trường tên, email, mật khẩu và gửi dữ liệu đến một API.
+Sử dụng thư viện http để truy cập API và lấy về dữ liệu về thời tiết và hiển thị nó trên màn hình.
+*/
